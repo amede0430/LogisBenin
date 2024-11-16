7f6fbf9ee220e79678d90710f34a8fb02fc99313
hermann2001 <adreanodalmeida@gmail.com>
2024-11-16 11:30:24 +0100

    contact

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -138,7 +138,7 @@ export function Home() {
       </section> */}
       <section className="relative bg-white py-24 px-4">
         <div className="container mx-auto">
-          <PageTitle section="Co-Working" heading="Build something">
+          {/* <PageTitle section="Co-Working" heading="Build something">
             Put the potentially record low maximum sea ice extent tihs year down
             to low ice. According to the National Oceanic and Atmospheric
             Administration, Ted, Scambos.
@@ -164,7 +164,7 @@ export function Home() {
                 </Typography>
               </Card>
             ))}
-          </div>
+          </div> */}
           <PageTitle section="Contact Us" heading="Want to work with us?">
             Complete this form and we will get back to you in 24 hours.
           </PageTitle>
